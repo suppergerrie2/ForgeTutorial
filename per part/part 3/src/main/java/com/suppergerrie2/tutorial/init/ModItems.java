071edf1ce98e9fadb8cb63d15d934574fd68c3b9
suppergerrie2 <gerjan2001@gmail.com>
2017-07-18 12:13:04 +0200

    Minecraft modding with forge - 3 - custom item
    In this part we will add our first custom item, it will not do anything except exist right now but it is an item!

---
+++ per part/part 3/src/main/java/com/suppergerrie2/tutorial/init/ModItems.java
@@ -0,0 +1,36 @@
+package com.suppergerrie2.tutorial.init;
+
+import com.suppergerrie2.tutorial.Reference;
+import com.suppergerrie2.tutorial.items.ItemBasic;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber(modid=Reference.MODID)
+public class ModItems {
+	
+	static ItemBasic tutorialItem;
+	
+	public static void init() {
+		tutorialItem = new ItemBasic("tutorialItem");
+	}
+	
+	@SubscribeEvent
+	public static void registerItems(RegistryEvent.Register<Item> event) {
+		event.getRegistry().registerAll(tutorialItem);
+	}
+	
+	@SubscribeEvent
+	public static void registerRenders(ModelRegistryEvent event) {
+		registerRender(tutorialItem);
+	}
+	
+	private static void registerRender(Item item) {
+		ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation( item.getRegistryName(), "inventory"));
+	}
+}
---
+++ per part/part 3/src/main/java/com/suppergerrie2/tutorial/items/ItemBasic.java
@@ -0,0 +1,11 @@
+package com.suppergerrie2.tutorial.items;
+
+import net.minecraft.item.Item;
+
+public class ItemBasic extends Item {
+	
+	public ItemBasic(String name) {
+		setUnlocalizedName(name);
+		setRegistryName(name);
+	}	
+}
