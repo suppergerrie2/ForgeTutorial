0ef6f0be21a8ac2aa0d913dfb118f1efc01e2aa5
suppergerrie2 <gerjan2001@gmail.com>
2019-02-24 15:34:38 +0100

    Minecraft 1.13 modding with Forge – 2 – Setting up basic mod

---
+++ src/main/java/com/suppergerrie2/tutorial/Reference.java
@@ -0,0 +1,8 @@
+package com.suppergerrie2.tutorial;
+
+public class Reference {
+
+    public static final String MODID = "sg2tutorial";
+
+
+}
---
+++ src/main/java/com/suppergerrie2/tutorial/TutorialMod.java
@@ -0,0 +1,7 @@
+package com.suppergerrie2.tutorial;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Reference.MODID)
+public class TutorialMod {
+}
