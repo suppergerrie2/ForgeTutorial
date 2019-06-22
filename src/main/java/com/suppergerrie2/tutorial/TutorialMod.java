3c64aeebc41893b357d0bedb105cf808070fcee5
suppergerrie2 <gerjan2001@gmail.com>
2019-06-22 20:45:34 +0200

    part 2

---
+++ src/main/java/com/suppergerrie2/tutorial/TutorialMod.java
@@ -0,0 +1,18 @@
+package com.suppergerrie2.tutorial;
+
+import static com.suppergerrie2.tutorial.TutorialMod.MOD_ID;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * The main class of the mod, this is the class that looks like a mod to forge.
+ */
+@Mod(MOD_ID)
+public class TutorialMod {
+
+    /**
+     * The modid of this mod, this has to match the modid in the mods.toml and has to be in the format defined in {@link net.minecraftforge.fml.loading.moddiscovery.ModInfo}
+     */
+    public static final String MOD_ID = "stutorialmod";
+
+}
