7e74b5aff6a15288c91cdcadb5e98e543bec23d1
suppergerrie2 <gerjan2001@gmail.com>
2020-02-14 11:34:42 +0100

    Minecraft 1.15 modding with Forge – 2 – Setting up a basic mod

---
+++ src/main/java/com/suppergerrie2/stutorialmod/TutorialMod.java
@@ -0,0 +1,10 @@
+package com.suppergerrie2.stutorialmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TutorialMod.MOD_ID)
+public class TutorialMod {
+
+    public static final String MOD_ID = "stutorialmod";
+
+}
